1a4bebcbe0ba06ade0260b30e92d0d61d4ee3cae
Night2wolf <trevor.klinkenberg@gmail.com>
2020-05-03 14:38:27 -0500

    Data set and question 3

---
+++ CNNModel.py
@@ -0,0 +1,17 @@
+from keras import models, layers
+
+class CNNModel:
+    @staticmethod
+    def generateModel(input):
+        model = models.Sequential()
+        model.add(layers.Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=input.shape[1:]))
+        model.add(layers.MaxPool2D(pool_size=(2, 2)))
+        model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+        model.add(layers.MaxPool2D(pool_size=(2, 2)))
+        model.add(layers.Dropout(rate=0.2))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(256, activation='relu'))
+        model.add(layers.Dropout(rate=0.5))
+        model.add(layers.Dense(8, activation='softmax'))
+        model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+        return model
